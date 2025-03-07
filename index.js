606089e7421bdad36aa0184616cb71f268ea4dac
Elijah <elijah.macharia@student.moringaschool.com>
2025-03-07 15:26:30 +0300

    Arrays

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
+let random = Math.floor(Math.random()*8);
+console.log(random);
+const num3 = 64
+const num4 = 10
+let mod = num3 % num4;
+console.log(mod);
+const max = Math.max(4,5,8,9,20)
